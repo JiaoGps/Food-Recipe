6bb701c2b3f843c8078807889501ef2e4f6baab7
JiaoGps <1078891480@qq.com>
2021-04-06 22:50:51 +0800

    首次提交

---
+++ pages/home/home.js
@@ -0,0 +1,180 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    TabCur: 0,
+    scrollLeft: 0,
+    cardCur: 0,
+    tabList:[{
+      id:0,
+      text:"Pizza"
+    },
+    {
+      id:1,
+      text:"Chicken"
+    },
+    {
+      id:2,
+      text:"Fish"
+    },
+    {
+      id:3,
+      text:"Salad"
+    },
+    {
+      id:4,
+      text:"Beef"
+    },
+    {
+      id:5,
+      text:"Michelin"
+    }
+    ],
+    swiperList: [{
+      id: 0,
+      type: 'image',
+      url: 'https://wx3.sinaimg.cn/mw690/001O3MZugy1goyfcttsttj60og0ogjsn02.jpg'
+    }, {
+      id: 1,
+      type: 'image',
+      url: 'https://wx3.sinaimg.cn/mw690/001O3MZugy1goyfcu2anij60og10oab202.jpg',
+    }, {
+      id: 2,
+      type: 'image',
+      url: 'https://wx1.sinaimg.cn/mw690/001O3MZugy1goyfcu6vhmj60og0uktaq02.jpg'
+    }, {
+      id: 3,
+      type: 'image',
+      url: 'https://wx3.sinaimg.cn/mw690/001O3MZugy1goyfcufsqtj60og0gamxk02.jpg'
+    }],
+  },
+  tabSelect(e) {
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    this.towerSwiper('swiperList');
+    // 初始化towerSwiper 传已有的数组名即可
+  },
+  DotStyle(e) {
+    this.setData({
+      DotStyle: e.detail.value
+    })
+  },
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  // towerSwiper
+  // 初始化towerSwiper
+  towerSwiper(name) {
+    let list = this.data[name];
+    for (let i = 0; i < list.length; i++) {
+      list[i].zIndex = parseInt(list.length / 2) + 1 - Math.abs(i - parseInt(list.length / 2))
+      list[i].mLeft = i - parseInt(list.length / 2)
+    }
+    this.setData({
+      swiperList: list
+    })
+  },
+  // towerSwiper触摸开始
+  towerStart(e) {
+    this.setData({
+      towerStart: e.touches[0].pageX
+    })
+  },
+  // towerSwiper计算方向
+  towerMove(e) {
+    this.setData({
+      direction: e.touches[0].pageX - this.data.towerStart > 0 ? 'right' : 'left'
+    })
+  },
+  // towerSwiper计算滚动
+  towerEnd(e) {
+    let direction = this.data.direction;
+    let list = this.data.swiperList;
+    if (direction == 'right') {
+      let mLeft = list[0].mLeft;
+      let zIndex = list[0].zIndex;
+      for (let i = 1; i < list.length; i++) {
+        list[i - 1].mLeft = list[i].mLeft
+        list[i - 1].zIndex = list[i].zIndex
+      }
+      list[list.length - 1].mLeft = mLeft;
+      list[list.length - 1].zIndex = zIndex;
+      this.setData({
+        swiperList: list
+      })
+    } else {
+      let mLeft = list[list.length - 1].mLeft;
+      let zIndex = list[list.length - 1].zIndex;
+      for (let i = list.length - 1; i > 0; i--) {
+        list[i].mLeft = list[i - 1].mLeft
+        list[i].zIndex = list[i - 1].zIndex
+      }
+      list[0].mLeft = mLeft;
+      list[0].zIndex = zIndex;
+      this.setData({
+        swiperList: list
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
